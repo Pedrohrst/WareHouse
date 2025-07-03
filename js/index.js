71fe24e63bd2a3f8647f9b3ff3e0c8ef0904aea4
Pedrohrst <lofsbatman@gmail.com>
2025-07-03 14:22:36 -0300

    [Assets] colocando as img internamente e trocando a função de buscar imagem no banco. Motivo: Lentidão no Sistema. Solução: futuramente conseguir outro DB com um pouco mais de memoria para realizar testes.

--- js/index.js
+++ js/index.js
@@ -28,4 +28,44 @@ document.addEventListener("DOMContentLoaded", () => {
     modal.classList.add("hidden");
     formLogin.classList.add("hidden");
   });
+
+  async function carregarProdutos() {
+    try {
+      const resposta = await fetch("https://apisware.onrender.com/produto/todos");
+      const produtos = await resposta.json();
+
+      mostrarProdutos(produtos);
+    } catch (erro) {
+      console.error("Erro ao carregar produtos:", erro);
+    }
+  }
+
+  function mostrarProdutos(produtos) {
+    const container = document.getElementById("produtos");
+    container.querySelectorAll("article.produto").forEach(el => el.remove());
+
+    const primeiros10 = produtos.slice(0, 10);
+
+    primeiros10.forEach(produto => {
+      //const imgUrl = `https://apisware.onrender.com/produto/imagem/${produto.id}`; muito pesado fazer essa consulta para buscar no banco.
+
+      const imgNome = produto.nome.replace(/\s/g, "").toLowerCase(); // remove espaços
+      console.log("nome: " + imgNome);
+      const imgUrl = `assets/${imgNome}.png`; //solução temporaria até pegar um DB maior
+
+      const artigo = document.createElement("article");
+      artigo.classList.add("produto");
+
+      artigo.innerHTML = `
+      <h3>${produto.nome}</h3>
+      <img src="${imgUrl}" alt="${produto.nome}" />
+      <p>${produto.descricao || "Sem descrição"}</p>
+      <p><strong>Preço:</strong> R$ ${produto.preco.toFixed(2)}</p>
+    `;
+
+      container.appendChild(artigo);
+    });
+  }
+
+  carregarProdutos();
 });
